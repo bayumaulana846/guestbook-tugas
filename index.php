6699ca811ae72269aa19dc23ea30eeaa917dd3a1
devi20 <devikhoirunnisa2009@gmail.com>
2018-11-05 08:15:04 +0700

    perbarui index

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php
-phpinfo();
-?>
+<html>
+<head><title>Guestbook</title>
+</head>
+<body bgcolor="darkred" text="white">
+  <div id="admin" >
+    <h1>Buku Tamu</h1>
+    <p>silahkan isi daftar tamu</p>
+    <form action=""method="post">
+    <p><b>Nama Lengkap:</b><br><input type="text" name="name" placeholder=""></p>
+    <p><b>Email:</b><br><input type="text" name="email" placeholder=""></p>
+    <p><b>No.Telp:</b><br><input type="text" name="No.Telp" placeholder=""></p>
+    <p><b>pesan:</b><br><input type="pesan" name="pesan" rows="5"cols="50" placeholder=""></p>
+    <p><input type="submit" nama="go" value="Kirim"/><input type="reset" nama="del" value="Hapus"/></p>
+  </form>
+</div>
+</body>
+</html>
